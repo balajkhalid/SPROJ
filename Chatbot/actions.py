04b2d1f68c5cc840d320aa53b11efae5298e8481
Balaj Khalid <balaj.khalid@ymail.com>
2021-05-20 23:31:10 +0500

    Updated actions

--- Chatbot/actions.py
+++ Chatbot/actions.py
@@ -115,23 +115,32 @@ class ActionFindCases(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         distype = "None"
-        location = "None"
         condition = "None"
+        condition.lower()
         distype = tracker.get_slot("disaster_type")
-        location = tracker.get_slot("location")
         condition = tracker.get_slot("patient_condition")
 
+        # access api
+        URL = "https://coronavirus-19-api.herokuapp.com/countries/pakistan"
+        r = requests.get(url=URL)
+
+        # extracting data in json format
+        data = r.json()
+
         if condition == "infected" or condition == "infect" or condition == "infection":
-            response = "Currently the number of "+distype+" victims in your location is "
+            response = "Currently the number of "+distype+" victims in Pakistan " + data["todayCases"]
 
         elif condition == "died" or condition == "death" or condition == "dying" or condition == "deaths":
-            response = "Currently the number of "+distype+" victims in your location is "
+            response = "Currently the number of "+distype+" victims in Pakistan" + data["todayDeaths"]
 
         elif condition == "recovered" or condition == "recoveries" or condition == "recover":
-            response = "Currently the number of "+distype+" recoveries in your location is "
+            response = "Currently the number total of "+distype+" recoveries in Pakistan " + data["recovered"]
+
+        elif condition == "critical" :
+            response = "Currently the number total of "+distype+" critical patients in Pakistan " + data["critical"]
 
         elif condition == "None":
-            response = "Currently, in your location the cases of the following are:\nDeaths: Balaj pls add API\nRecovered Patients: Balaj pls add API\nInfected Patients: Balaj pls add API\nCritical Condition: Balaj pls add API"
+            response = "Currently, in Pakistan the cases of the following are:\nDeaths:" + data["todayDeaths"] + "\nRecovered Patients:" + data["recovered"] + "\nInfected Patients: " +  data["todayCases"] +"\nCritical Condition:" + data["critical"]
 
         dispatcher.utter_message(text=response)
 
@@ -173,8 +182,7 @@ class ActionFindMed(Action):
 
         distype = tracker.get_slot("disaster_type")
 
-        response = "There is currently no universal cure available for " + distype + \
-            ", but you can look at the following links to read more about the medical developments related to this topic"
+        response = "According to WHO:\nScientists around the world are working to find and develop treatments for COVID-19.\nOptimal supportive care includes oxygen for severely ill patients and those who are at risk for severe disease and more advanced respiratory support such as ventilation for patients who are critically ill.\nDexamethasone is a corticosteroid that can help reduce the length of time on a ventilator and save lives of patients with severe and critical illness. Read our dexamethasone Q&A for more information.\nResults from the WHO’s Solidarity Trial indicated that remdesivir, hydroxychloroquine, lopinavir/ritonavir and interferon regimens appear to have little or no effect on 28-day mortality or the in-hospital course of COVID-19 among hospitalized patients.\nHydroxychloroquine has not been shown to offer any benefit for treatment of COVID-19. Read our hydroxychloroquine Q&A for more information.\nWHO does not recommend self-medication with any medicines, including antibiotics, as a prevention or cure for COVID-19. WHO is coordinating efforts to develop treatments for COVID-19 and will continue to provide new information as it becomes available.\nFind more details at: https://www.who.int/emergencies/diseases/novel-coronavirus-2019/question-and-answers-hub/q-a-detail/coronavirus-disease-covid-19#"
         dispatcher.utter_message(text=response)
 
         return[]
@@ -192,8 +200,9 @@ class ActionFindTest(Action):
         distype = tracker.get_slot("disaster_type")
 
         response = "If you want to get tested for "+distype + \
-            " the following tests are available for you:"
+            " the following tests are available for you:\nRT-PCR\nAntigen test\nAntibody test"
         dispatcher.utter_message(text=response)
+        dispatcher.utter_message(image="https://imgur.com/a/q0TeP63")
 
         return[]
 
@@ -232,25 +241,25 @@ class ActionListPrevent(Action):
         return[]
 
 
-class ActionConfirmCause(Action):
+# class ActionConfirmCause(Action):
 
-    def name(self) -> Text:
-        return "action_confirm_cause"
+#     def name(self) -> Text:
+#         return "action_confirm_cause"
 
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        check_cause = "None"
-        distype = tracker.get_slot("disaster_type")
-        check_cause = tracker.get_slot("cause")
-        if check_cause == "None":
-            check_cause = tracker.latest_message['text']
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#         check_cause = "None"
+#         distype = tracker.get_slot("disaster_type")
+#         check_cause = tracker.get_slot("cause")
+#         if check_cause == "None":
+#             check_cause = tracker.latest_message['text']
 
-        response = " According to blah blah " + \
-            check_cause + " does/doesn't cause " + distype
-        dispatcher.utter_message(text=response)
+#         response = " According to blah blah " + \
+#             check_cause + " does/doesn't cause " + distype
+#         dispatcher.utter_message(text=response)
 
-        return[]
+#         return[]
 
 
 class ActionAskCause(Action):
@@ -269,9 +278,7 @@ class ActionAskCause(Action):
         return[]
 
 # ML model query
-
-
-class ActionML(Action):
+class ActionConfirmCause(Action):
     def name(self) -> Text:
         return "action_ml"
 
@@ -279,14 +286,20 @@ class ActionML(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
+        distype = tracker.get_slot("disaster_type")
+        distype.lower()
         query = tracker.get_slot('query')  # obtain location
 
-        client = Algorithmia.client(algorthimia_key)
-        algo = client.algo(algorthimia_api)
-        algo.set_options(timeout=300)  # optional
-        res = algo.pipe(input).result
-        response = "According to our ML model your query is " + res
-        dispatcher.utter_message(text=response)
+        if distype == "covid-19" or distype == "corona" or distype == "coronavirus" or distype == "covid" or distype == "covid19" or distype == "corona virus": 
+            client = Algorithmia.client(algorthimia_key)
+            algo = client.algo(algorthimia_api)
+            algo.set_options(timeout=300)  # optional
+            res = algo.pipe(input).result
+            response = "According to our ML model your query is " + res
+            dispatcher.utter_message(text=response)
+        else:
+            response = "Currently there is no machine learning model for this disaster type. You can visit our Forums page to get an answer to your query, https://corona-info.flywheelsites.com/home/forums/"
+            dispatcher.utter_message(text=response)
 
         return[]
 
@@ -301,6 +314,8 @@ class ActionDB(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
+        distype = tracker.get_slot("disaster_type")
+        distype.lower()
         query = tracker.get_slot('query')  # obtain location
 
         # breaking query
@@ -316,8 +331,12 @@ class ActionDB(Action):
                 filtered_query.pop()
 
         # quering
-        query_result = corona_faqs.where(
-            u'keywords', u'array_contains_any', filtered_query).stream()
+        if distype == "covid-19" or distype == "corona" or distype == "coronavirus" or distype == "covid" or distype == "covid19" or distype == "corona virus": 
+            query_result = corona_faqs.where(
+                u'keywords', u'array_contains_any', filtered_query).stream()
+        else: 
+            response = "Currently there are no FAQs for this disaster type. You can visit our Forums page to get an answer to your query, https://corona-info.flywheelsites.com/home/forums/"
+            dispatcher.utter_message(text=response)
 
         # finding item whose keywords match the most
         ans = ""
